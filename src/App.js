cb162f156feae38aca2cddbdca6f26d274cb6bbc
Muhammed-Shahid <shahid04m@gmail.com>
2023-06-02 09:19:38 +0530

    main

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './Components/NavBar/Navbar';
 import Banner from './Components/Banner/Banner';
 import About from './Components/About/About';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
@@ -11,8 +12,38 @@ function App() {
    <Banner/>
    <section className='abtSec'>
  <About/>
-
  </section>
+
+
+ <section className="universe">
+  <div className="uniContainer">
+    <h1 style={{fontSize:'34px'}} >Zerodha Universe</h1>
+
+    <p style={{fontSize:'24px'}} >A whole ecosystem of modern investment apps <br />
+tailored to specific needs, built from the ground up</p>
+
+<div className="uniImg">
+  <img style={{width:'70%'}} src={process.env.PUBLIC_URL + '/universe.png'} alt="" />
+</div>
+  </div>
+ </section>
+
+
+<section className="pricing">
+  <div className="pricingLft">
+<h1 style={{fontSize:'26px'}}>Unbeatable Pricing</h1>
+
+<p style={{fontSize:'24px'}}>We pioneered the concept of discount broking and price <br /> transparency in India. Flat fees and no hidden charges.</p>
+  </div>
+
+  <div className="pricingRight">
+  <img style={{width:'70%'}} src={process.env.PUBLIC_URL + '/pricing2.png'} alt="" />
+
+  </div>
+</section>
+
+
+<Footer/>
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,128 @@
+
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+<footer id="footer">
+  <div className="container">
+    <div className="row between main-footer">
+      <div className="columns three">
+        <div className="footer-logo">
+          <img src="/static/images/logo.svg" alt />
+        </div>
+      
+        <p className="copyright text-grey">© 2010 - 2023, Zerodha Broking Ltd.</p>
+        <p className="copyright text-grey">All rights reserved.</p>
+        <ul className="social">
+          <li>
+            <a target="_blank" href="https://twitter.com/zerodhaonline"><i className="icon-twitter" /></a>
+          </li>
+          <li>
+            <a target="_blank" href="https://facebook.com/zerodha.social"><i className="icon-facebook-official" /></a>
+          </li>
+          <li>
+            <a target="_blank" href="https://instagram.com/zerodhaonline/"><i className="icon-instagram" /></a>
+          </li>
+          <li>
+            <a target="_blank" href="https://linkedin.com/company/zerodha">
+              <i className="icon-linkedin" />
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="https://t.me/zerodhain">
+              <i className="icon-telegram" />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="columns three">
+        <ul className="list-style">
+          <li className="nav-head">Company</li>
+          <li><a href="https://zerodha.com/about/">About</a></li>
+          <li><a href="https://zerodha.com/products/">Products</a></li>
+          <li><a href="https://zerodha.com/pricing/">Pricing</a></li>
+          <li>
+            <a href="https://zerodha.com/refer/">Referral programme</a>
+          </li>
+          <li><a href="https://careers.zerodha.com">Careers</a></li>
+          <li><a href="https://zerodha.tech">Zerodha.tech</a></li>
+          <li><a href="https://zerodha.com/media/">Press &amp; media</a></li>
+          <li><a href="https://zerodha.com/cares/">Zerodha Cares (CSR)</a></li>
+        </ul>
+      </div>
+      <div className="columns three">
+        <ul className="list-style">
+          <li className="nav-head">Support</li>
+          <li><a href="https://zerodha.com/contact/">Contact us</a></li>
+          <li>
+            <a href="https://support.zerodha.com">Support portal</a>
+          </li>
+          <li><a href="https://zerodha.com/z-connect/">Z-Connect blog</a></li>
+          <li><a href="https://zerodha.com/charges/">List of charges</a></li>
+          <li>
+            <a href="https://zerodha.com/resources/">Downloads &amp; resources</a>
+          </li>
+          <li>
+            <a href="/videos">Videos</a>
+          </li>
+          <li>
+            <a href="https://stocks.zerodha.com">Market overview</a>
+          </li>
+        </ul>
+      </div>
+      <div className="columns three">
+        <ul className="list-style">
+          <li className="nav-head">Account</li>
+          <li>
+            <a href="https://zerodha.com/open-account/">Open an account</a>
+          </li>
+          <li>
+            <a href="https://zerodha.com/fund-transfer/">Fund transfer</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div className="footer-graveyard-links text-center">
+      <ul>
+        <li>
+          <a href="https://nseindia.com">NSE</a>
+        </li>
+        <li>
+          <a href="https://www.bseindia.com/">BSE</a>
+        </li>
+        <li>
+          <a href="https://www.mcxindia.com/">MCX</a>
+        </li>
+        <li>
+          <a href="https://zerodha.com/terms-and-conditions/">Terms &amp; conditions</a>
+        </li>
+        <li>
+          <a href="https://zerodha.com/policies-and-procedures/">Policies &amp; procedures</a>
+        </li>
+        <li>
+          <a href="https://zerodha.com/privacy-policy/">Privacy policy</a>
+        </li>
+        <li>
+          <a href="https://zerodha.com/disclosure/">Disclosure</a>
+        </li>
+        <li>
+          <a href="https://zerodha.com/investor-attention/">For investor's attention</a>
+        </li>
+        <li>
+          <a href="https://zerodha.com/tos/investor-charter/">Investor charter</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</footer>
+
+
+
+    </div>
+  )
+}
+
+export default Footer
+
